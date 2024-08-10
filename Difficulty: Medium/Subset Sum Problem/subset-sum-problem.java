ec4fcb6b1d067e90f835be1c5f2b83a0badba366
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-11 00:08:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -36,26 +36,22 @@ class GFG
 class Solution{
 
 
-    static Boolean isSubsetSum(int n, int arr[], int sum){
+    static Boolean isSubsetSum(int n, int arr[], int s){
         // code here
-        boolean[][] dp = new boolean[n][sum+1];
-        for(int i =0;i<n;i++){
+        boolean[][] dp = new boolean[n][s+1];
+        for(int i = 0;i<n;i++){
             dp[i][0] = true;
         }
-        if(arr[0] <= sum){
-           dp[0][arr[0]] = true; 
-        }
         
-        for(int ind = 1;ind<n;ind++){
-            for(int t = 1;t<=sum;t++){
-                boolean notTake = dp[ind-1][t];
+        if(arr[0] <= s) dp[0][arr[0]] = true;
+        for(int i =1;i<n;i++){
+            for(int sum = 1;sum<=s;sum++){
                 boolean take = false;
-                if(t >= arr[ind]){
-                    take = dp[ind-1][t-arr[ind]];
-                }
-                dp[ind][t] = take || notTake;
+                boolean notTake = dp[i-1][sum];
+                if(sum >= arr[i]) take = dp[i-1][sum - arr[i]];
+                dp[i][sum] = take || notTake;
             }
         }
-        return dp[n-1][sum];
+        return dp[n-1][s];
     }
 }
