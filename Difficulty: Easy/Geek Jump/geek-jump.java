7d7a34a737e254e7baf24147b87b2705fcfd5fd2
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-10 18:40:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Geek Jump/geek-jump.java
+++ Difficulty: Easy/Geek Jump/geek-jump.java
@@ -33,14 +33,15 @@ class GFG{
 class Solution{
     public int minimumEnergy(int arr[],int n){
         //code here
-        int[] dp = new int[n+1];
+        int [] dp = new int[n];
         dp[0] = 0;
-        for(int i =1;i<n;i++){
-            int fs = dp[i-1] + Math.abs(arr[i] - arr[i-1]);
+        for(int i = 1;i<n;i++){
+            int fs = Math.abs(arr[i] - arr[i-1]) + dp[i-1];
             int ss = Integer.MAX_VALUE;
-            if(i > 1){
-                ss = dp[i-2] + Math.abs(arr[i] - arr[i-2]);
+            if(i>1){
+                ss = Math.abs(arr[i] - arr[i-2]) + dp[i-2];
             }
+            
             dp[i] = Math.min(fs, ss);
         }
         return dp[n-1];
