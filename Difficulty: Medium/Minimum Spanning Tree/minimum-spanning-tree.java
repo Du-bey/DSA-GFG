7e6815c6239753998807241da05b3ab1b7e30c8a
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-29 02:18:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -38,48 +38,102 @@ public class Main {
 // User function Template for Java
 
 class Solution {
-    static int spanningTree(int V, int E, List<List<int[]>> adj) {
-        // Code Here.
-        boolean[] visited = new boolean[V];
-        PriorityQueue<Pair> minHeap = new PriorityQueue<>(Comparator.comparingInt(p -> p.weight));
-        
-        minHeap.offer(new Pair(0, 0));
-        int totalWeight = 0;
-
-        while (!minHeap.isEmpty()) {
-            Pair current = minHeap.poll();
-            int node = current.node;
-            int weight = current.weight;
-
-            if (visited[node]) continue;
-
-            visited[node] = true;
-
-            totalWeight += weight;
-
-            // Explore all the adjacent vertices
-            for (int[] neighbor : adj.get(node)) {
-                int adjacentNode = neighbor[0];
-                int edgeWeight = neighbor[1];
-                if (!visited[adjacentNode]) {
-                    minHeap.offer(new Pair(adjacentNode, edgeWeight));
-                }
+    static int spanningTree(int v, int e, List<List<int[]>> adj) {
+        ArrayList<Trio> edges=new ArrayList<>();
+        for(int i=0;i<adj.size();i++){
+            List<int[]> node=adj.get(i);
+            for(int[] arr:node){
+                int nei=arr[0];
+                int wei=arr[1];
+                edges.add(new Trio(wei,i,nei));
             }
         }
-        return totalWeight;
+        Collections.sort(edges);
+        
+        //now make the minimum spanning tree
+        DisjointSet ds=new DisjointSet(v);
+        int sumOfWei=0;
+        for(int i=0;i<edges.size();i++){
+            Trio edge=edges.get(i);
+            int wei=edge.wei;
+            int node=edge.node;
+            int nei=edge.nei;
+            if(ds.unionBySize(node,nei)){
+                sumOfWei+=wei;
+            }
+        }
+        return sumOfWei;
     }
 }
-class Pair {
+class DisjointSet{
+    ArrayList<Integer> par=new ArrayList<>();
+    ArrayList<Integer> rank=new ArrayList<>();
+    ArrayList<Integer> size=new ArrayList<>();
+    
+    DisjointSet(int n){
+        for(int i=0;i<n;i++){
+            par.add(i);
+            rank.add(0);
+            size.add(1);
+        }
+    }
+    
+    int findUltPar(int node){
+        if(par.get(node)==node){
+            return node;
+        }
+        int ultPar=findUltPar(par.get(node));
+        par.set(node,ultPar);
+        return par.get(node);
+    }
+    
+    boolean unionByRank(int u,int v){
+        int parU=findUltPar(u);
+        int parV=findUltPar(v);
+        if(parU==parV){
+            return false;
+        }
+        int rankParU=rank.get(parU);
+        int rankParV=rank.get(parV);
+        if(rankParU>rankParV){
+            par.set(parV,parU);
+        }else if(rankParV>rankParU){
+            par.set(parU,parV);
+        }else{
+            par.set(parU,parV);
+            rank.set(parV,parV+1);
+        }
+        return true;
+    }
+    
+    boolean unionBySize(int u,int v){
+        int parU=findUltPar(u);
+        int parV=findUltPar(v);
+        if(parU==parV){
+            return false;
+        }
+        int sizeParU=size.get(parU);
+        int sizeParV=size.get(parV);
+        if(sizeParU>sizeParV){
+            par.set(parV,parU);
+            size.set(parU,parU+parV);
+        }else{
+            par.set(parU,parV);
+            size.set(parV,parU+parV);
+        }
+        return true;
+    }
+}
+class Trio implements Comparable<Trio>{
+    int wei;
     int node;
-    int weight;
-
-    Pair(int node, int weight){
-        this.node = node;
-        this.weight = weight;
+    int nei;
+    Trio(int wei,int node,int nei){
+        this.wei=wei;
+        this.node=node;
+        this.nei=nei;
+    }
+    public int compareTo(Trio o){
+        return this.wei-o.wei;
     }
 }
-    
-    
-    
-    
-    
