607e313a089139ac69e7f5087da258c524252908
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-11 01:57:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Circle of strings/circle-of-strings.java
+++ Difficulty: Hard/Circle of strings/circle-of-strings.java
@@ -25,39 +25,48 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    void dfs(int i, List<List<Integer>> graph, boolean[] visit) {
-        visit[i] = true;
-        for(int idx: graph.get(i))
-            if(visit[idx] != true)
-                dfs(idx,graph,visit);
-    }
-
     public int isCircle(String arr[]) {
-        
-        int in[] = new int[26];
-        int out[] = new int[26];
+        // code here
+        int n = arr.length;
+        String s1 = arr[0];
+        char c1 = s1.charAt(0);
+        int st = c1 - 'a';
+        int[] in = new int[26];
+        int[] out = new int[26];
         List<List<Integer>> adj = new ArrayList<>();
+        for(int i =0;i<26;i++){
+            adj.add(new ArrayList<>());
+        }
         
-        for(int i=0; i<26; i++) adj.add(new ArrayList<>());
-         
-        for(String s: arr) {
-            int n = s.length();
-            in[s.charAt(0) - 'a']++;
-            out[s.charAt(n-1)-'a']++;
-            adj.get(s.charAt(0)-'a').add(s.charAt(n-1)-'a');
+        for(String s : arr){
+            char c = s.charAt(0);
+            char c2 = s.charAt(s.length() - 1);
+            int u = c - 'a';
+            int v = c2 - 'a';
+            
+            in[v]++;
+            out[u]++;
+            adj.get(u).add(v);
         }
         for(int i=0; i<26; i++) {
             if(in[i] != out[i]) return 0;
         }
-        boolean visit[] = new boolean[26];
-        
-        dfs(arr[0].charAt(0)-'a',adj,visit);
-        
+        boolean[] vis = new boolean[26];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(st);
+        while(!q.isEmpty()){
+            int u = q.poll();
+            vis[u] = true;
+            for(int v : adj.get(u)){
+                if(!vis[v]){
+                    q.add(v);
+                }
+            }
+        }
         for(int i=0; i<26; i++) {
-            if(in[i] > 0 && visit[i] != true)
+            if(in[i] > 0 && vis[i] != true)
                 return 0;
         }
-       
-       return 1;
+        return 1;
     }
 }
