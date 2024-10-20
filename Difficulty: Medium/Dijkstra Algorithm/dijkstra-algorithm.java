7b2bcc0ab87ad0591548d2d2be37920eb22d31f9
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-10-20 15:11:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -1,50 +1,50 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
-class DriverClass
-{
+class iPair {
+    int first, second;
+
+    iPair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class DriverClass {
     public static void main(String args[]) throws IOException {
-
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
         while (t-- > 0) {
             String str[] = read.readLine().trim().split(" ");
             int V = Integer.parseInt(str[0]);
             int E = Integer.parseInt(str[1]);
-    
-            ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
-            for(int i=0;i<V;i++)
-            {
-                adj.add(new ArrayList<ArrayList<Integer>>());
+
+            ArrayList<ArrayList<iPair>> adj = new ArrayList<ArrayList<iPair>>();
+            for (int i = 0; i < V; i++) {
+                adj.add(new ArrayList<iPair>());
             }
-            
-            int i=0;
-            while (i++<E) {
+
+            int i = 0;
+            while (i++ < E) {
                 String S[] = read.readLine().trim().split(" ");
                 int u = Integer.parseInt(S[0]);
                 int v = Integer.parseInt(S[1]);
                 int w = Integer.parseInt(S[2]);
-                ArrayList<Integer> t1 = new ArrayList<Integer>();
-                ArrayList<Integer> t2 = new ArrayList<Integer>();
-                t1.add(v);
-                t1.add(w);
-                t2.add(u);
-                t2.add(w);
+                iPair t1 = new iPair(v, w);
+                iPair t2 = new iPair(u, w);
                 adj.get(u).add(t1);
                 adj.get(v).add(t2);
             }
-            
-            int S = Integer.parseInt(read.readLine());
-            
+
+            int src = Integer.parseInt(read.readLine());
+
             Solution ob = new Solution();
-            
-            int[] ptr = ob.dijkstra(V, adj, S);
-            
-            for(i=0; i<V; i++)
-                System.out.print(ptr[i] + " ");
+
+            ArrayList<Integer> res = ob.dijkstra(adj, src);
+
+            for (i = 0; i < V; i++) System.out.print(res.get(i) + " ");
             System.out.println();
         }
     }
@@ -52,31 +52,39 @@ class DriverClass
 // } Driver Code Ends
 
 
-//User function Template for Java
+/*
+class iPair {
+    int first, second;
 
+    iPair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+*/
 
-class Solution
-{
-    //Function to find the shortest distance of all the vertices
-    //from the source vertex S.
-    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int s)
-    {
+// User function Template for Java
+class Solution {
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex src.
+    ArrayList<Integer> dijkstra(ArrayList<ArrayList<iPair>> adj, int src) {
         // Write your code here
         PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.d - b.d);
-        pq.add(new Pair(0, s));
+        pq.add(new Pair(0, src));
+        int V = adj.size();
         int[] dis = new int[V];
         Arrays.fill(dis, (int) 1e9);
-        dis[s] = 0;
+        dis[src] = 0;
         while(!pq.isEmpty()){
             Pair p = pq.poll();
             int d = p.d;
             int u = p.node;
-            for(ArrayList<Integer> list : adj.get(u)){
-                int v = list.get(0);
-                int newd = list.get(1);
-                if(dis[v] > newd + d){
-                    dis[v] = newd + d;
-                    pq.add(new Pair(dis[v], v));
+            for(iPair pair : adj.get(u)){
+                int v = pair.first;
+                int newd = pair.second;
+                if(dis[v] > d + newd){
+                    dis[v] = d + newd;
+                    pq.add(new Pair(dis[v],v));
                 }
             }
         }
@@ -85,16 +93,19 @@ class Solution
                 dis[i] = -1;
             }
         }
-        return dis;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i : dis) {
+            list.add(i); 
+        }
+        return list;
     }
 }
 
-class Pair{
-    int d;
-    int node;
-    public Pair(int d, int node){
+class Pair {
+    int d, node;
+
+    Pair(int d, int node) {
         this.d = d;
         this.node = node;
     }
 }
-
