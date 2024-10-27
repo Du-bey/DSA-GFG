ddb3bc90039a336f357b5a63706c621e8de1358a
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-10-27 20:01:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,12 +1,12 @@
 //{ Driver Code Starts
 // Initial Template for Java
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.lang.*;
+import java.util.*;
+
 class GFG {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
             String[] s = br.readLine().trim().split(" ");
@@ -19,12 +19,11 @@ class GFG {
                 int u = Integer.parseInt(S[0]);
                 int v = Integer.parseInt(S[1]);
                 adj.get(u).add(v);
-                // adj.get(v).add(u);
+                adj.get(v).add(u); // Added this line to make the graph undirected
             }
             Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
-            for (int i = 0; i < ans.size(); i++)
-                System.out.print(ans.get(i) + " ");
+            ArrayList<Integer> ans = obj.bfsOfGraph(adj);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
             System.out.println();
         }
     }
@@ -33,13 +32,15 @@ class GFG {
 // } Driver Code Ends
 
 
+// User function Template for Java
 class Solution {
     // Function to return Breadth First Traversal of given graph.
-    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        // Code here
+    public ArrayList<Integer> bfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int n = adj.size();
         Queue<Integer> q = new LinkedList<>();
         ArrayList<Integer> ans = new ArrayList<>();
-        boolean[] vis = new boolean[V];
+        boolean[] vis = new boolean[n];
         q.add(0);
         vis[0] = true;
         while(!q.isEmpty()){
